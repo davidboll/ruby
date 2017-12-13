2aeda702cc06a006d817c3126fb23cc55234261f
bollen <david.boll@me.com>
2017-12-13 13:35:22 +0100

    First Ruby program

---
+++ my_group.rb
@@ -0,0 +1,11 @@
+# my_group-exersice
+
+my_group = []
+
+person_1 = {name: "David", gender: "male", age: 32}
+person_2 = {name: "Amanda", gender: "female", age: 25}
+person_3 = {name:"Andres", gender: "male", age: 33}
+
+my_group.push(person_1, person_2, person_3)
+
+puts my_group
