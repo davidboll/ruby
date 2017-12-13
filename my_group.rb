8a2354f360e2b82dcc5c7346c76833e01886b4fc
bollen <david.boll@me.com>
2017-12-13 14:23:32 +0100

    Program now iterates through array Added more clear comments

--- my_group.rb
+++ my_group.rb
@@ -1,11 +1,17 @@
 # my_group-exersice
 
+# Create a new array "my_group"
 my_group = []
 
-person_1 = {name: "David", gender: "male", age: 32}
-person_2 = {name: "Amanda", gender: "female", age: 25}
-person_3 = {name:"Andres", gender: "male", age: 33}
+# Creates new hashes of persons
+person_1 = { name: "David", gender: "guy", age: 32 }
+person_2 = { name: "Amanda", gender: "girl", age: 25 }
+person_3 = { name:"Andres", gender: "guy", age: 33 }
 
+# Adds the new persons to the array "my_group"
 my_group.push(person_1, person_2, person_3)
 
-puts my_group
+# Iterates through the array and displays the values stored in the hashes
+my_group.each do |i|
+  puts "#{i[:name]} is a #{i[:age]} year old #{i[:gender]}"
+end
