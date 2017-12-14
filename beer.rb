47dd0a0fb6be603b3a8fa1c91809072bad4a8c4d
bollen <david.boll@me.com>
2017-12-14 12:33:37 +0100

    Simple 99 bottles of beer..

---
+++ beer.rb
@@ -0,0 +1,12 @@
+puts "let's sing!"
+
+bottles = 99
+
+while bottles > 1
+  puts "#{bottles} bottles of beer on the wall"
+  bottles -= 1
+end
+
+puts "1 bottle of beer on the wall"
+
+puts "no moore beer :'("
